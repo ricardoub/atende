a9dc6470642122d30c1b03c420c7574208c22487
Ricardo Bomfim <ricardoub@gmail.com>
2016-11-27 13:32:17 -0200

    Usuários - alterações para trocar de branch
    1. Inclusão do menu principal
    2. alteração da rota home
    3. criação do grupo de rotas de usuários
    4. Criação do UserController
    5. Criação da view index

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home',     ['as' => 'home',   'uses' => 'HomeController@index']);
+
+Route::group(['prefix' => 'usuarios', 'as' => 'users.'], function() {
+  Route::get('',             ['as' => 'index',  'uses' => 'UserController@index']);
+
+  Route::get('create',       ['as' => 'create', 'uses' => 'UserController@create']);
+  Route::post('store',       ['as' => 'store',  'uses' => 'UserController@store']);
+
+  Route::get('edit/{id}',    ['as' => 'edit',   'uses' => 'UserController@edit']);
+  Route::post('update/{id}', ['as' => 'update', 'uses' => 'UserController@update']);
+
+  Route::get('delete/{id}',  ['as' => 'delete', 'uses' => 'UserController@destroy']);
+});
